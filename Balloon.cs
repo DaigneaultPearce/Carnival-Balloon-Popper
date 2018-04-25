4509468d940399aab62d32398cc0d95ab00f0962
Daigneault Pardo Pearce <Daigneault.pardopearce.515@my.csun.edu>
2018-04-25 02:56:48 -0700

    Commit only scripts untill I find solution

---
+++ Balloon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour {
+
+	public GameObject particle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Pop() {
+		Instantiate (particle, transform.position, transform.rotation, gameObject.transform.parent);
+		Destroy (gameObject);
+	}
+}
---
+++ GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public int balloonCount;
+	public GameObject balloon;
+	public Vector3 spawnValues;
+	public Material[] randomMaterials;
+	// Use this for initialization
+	void Start () {
+		SpawnBalloons ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SpawnBalloons(){
+		for (int i = 0; i < balloonCount; i++) {
+			Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, Random.Range (-spawnValues.z, spawnValues.z));
+			Quaternion spawnRotation = Quaternion.identity;
+
+			GameObject temp = Instantiate (balloon, spawnPosition, spawnRotation);
+			Renderer obj_renderer = temp.GetComponent<Renderer> ();
+			obj_renderer.material = randomMaterials [i % randomMaterials.Length];
+		}
+	}
+}
